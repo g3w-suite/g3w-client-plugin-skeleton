2186175156c3d70a96bce4ed184fc010a45e8d15
volterra79 <boccacci.francesco@gmail.com>
2022-07-12 11:49:33 +0200

    add base structure of plugin

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # g3w-client-plugin-base-template
+
+This is a base plugin template that can be used as template for create new G3W-CLIENT plugin repository.
---
+++ index.js
@@ -0,0 +1,44 @@
+import pluginConfig from './config';
+import Service from "./service";
+const {base, inherit} = g3wsdk.core.utils;
+const {Plugin} = g3wsdk.core.plugin;
+const {GUI} = g3wsdk.gui;
+const {addI18nPlugin} = g3wsdk.core.i18n;
+
+const _Plugin = function() {
+  base(this);
+  const {name, i18n} = pluginConfig;
+  this.name = name;
+  this.init = function() {
+    // add i18n of the plugin
+    addI18nPlugin({
+      name,
+      config: i18n
+    });
+    this.setService(Service);
+    //get config plugin from server
+    this.config = this.getConfig();
+    // check if has some condition default true
+    if (this.service.loadPlugin()) {
+      //plugin registry
+      if (this.registerPlugin(this.config.gid)) this.service.init(this.config);
+    }
+    // need to be call to hide loading icon on map
+    this.setReady(true);
+  };
+
+  this.load = function() {
+    this.init();
+  };
+
+  this.unload = function() {
+    this.service.clear()
+  }
+};
+
+inherit(_Plugin, Plugin);
+
+(function(plugin){
+  plugin.init();
+})(new _Plugin);
+
