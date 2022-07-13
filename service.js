e842709e1ac24f641510266a5f80205119851837
volterra79 <boccacci.francesco@gmail.com>
2022-07-13 09:53:51 +0200

    add service.js

---
+++ service.js
@@ -0,0 +1,19 @@
+const {base, inherit} = g3wsdk.core.utils;
+const {PluginService} = g3wsdk.core.plugin;
+
+function Service(){
+  base();
+  /**
+   * get plugin config object
+   * @param config
+   */
+  this.init = function(config={}){
+    /**
+     *  PLUGIN SERVICE INIT FUNCTION
+     */
+  }
+}
+
+inherit(Service, PluginService);
+
+export default new Service()
